f16969db57256bd343563d572620f772f6d69c5f
big20261028 <baekinki2026@gmail.com>
2026-01-29 23:47:51 +0900

    Title: a로 채워넣기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260129/a로 채워넣기/filling-with-a.py
+++ 260129/a로 채워넣기/filling-with-a.py
@@ -2,7 +2,7 @@ text = input()
 
 result = ""
 for idx in range(len(text)):
-    if idx == 2 or idx == (len(text)-2):
+    if idx == 1 or idx == (len(text)-2):
         result += 'a'
     else:
         result += text[idx]
