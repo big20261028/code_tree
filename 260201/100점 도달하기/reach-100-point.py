35bb2f286e67cdd1faee6fb027fcb4d86d70d951
big20261028 <baekinki2026@gmail.com>
2026-02-01 22:20:31 +0900

    Title: 100점 도달하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 260201/100점 도달하기/reach-100-point.py
+++ 260201/100점 도달하기/reach-100-point.py
@@ -1,10 +1,10 @@
 n = int(input())
 
 while n <= 100:
-    if n >= 90: print('A')
-    elif n >= 80: print('B')
-    elif n >= 70: print('C')
-    elif n >= 60: print('D')
-    else: print('F')
+    if n >= 90: print('A', end=' ')
+    elif n >= 80: print('B', end=' ')
+    elif n >= 70: print('C', end=' ')
+    elif n >= 60: print('D', end=' ')
+    else: print('F', end=' ')
 
     n += 1
