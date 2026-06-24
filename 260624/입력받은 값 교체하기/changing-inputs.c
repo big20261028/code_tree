297715d24daead977cdd03ec25264c94b799d98b
big20261028 <baekinki2026@gmail.com>
2026-06-24 13:58:06 +0900

    [Passed] 입력받은 값 교체하기 (1ms, 0MB)

---
+++ 260624/입력받은 값 교체하기/changing-inputs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+
+    int temp = a;
+    a = b;
+    b = temp;
+
+    printf("%d %d", a, b);
+
+    return 0;
+}
