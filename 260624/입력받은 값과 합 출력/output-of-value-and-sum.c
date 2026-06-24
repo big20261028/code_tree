a53b00245d0857e2f51328d1df37d94c1f901169
big20261028 <baekinki2026@gmail.com>
2026-06-24 13:59:17 +0900

    [Passed] 입력받은 값과 합 출력 (1ms, 0MB)

---
+++ 260624/입력받은 값과 합 출력/output-of-value-and-sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int A;
+    int B;
+
+    scanf("%d %d", &A, &B);
+
+    printf("%d %d %d", A, B, A+B);
+
+    return 0;
+}
