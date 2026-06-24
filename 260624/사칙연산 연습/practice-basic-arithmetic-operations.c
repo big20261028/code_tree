38bcfe0435e3c3823f91271c49f395ae968a7a44
big20261028 <baekinki2026@gmail.com>
2026-06-24 12:36:38 +0900

    [Passed] 사칙연산 연습 (1ms, 0MB)

---
+++ 260624/사칙연산 연습/practice-basic-arithmetic-operations.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a = 5;
+    int b = 30;
+    int c = a + b;
+
+    printf("%d\n%d\n",c / 8, c % 8);
+    return 0;
+}
