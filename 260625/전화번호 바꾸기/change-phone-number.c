ffebc8fc5718e99f0367a5d27fa70a2f82f25657
big20261028 <baekinki2026@gmail.com>
2026-06-25 15:18:44 +0900

    [Passed] 전화번호 바꾸기 (2ms, 0MB)

---
+++ 260625/전화번호 바꾸기/change-phone-number.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int front, end;
+    scanf("010-%d-%d", &front, &end);
+    printf("010-%d-%d", end, front);
+    return 0;
+}
