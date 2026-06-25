3858fc0189d475eae3f5d58fa100f9d11a56e8b6
big20261028 <baekinki2026@gmail.com>
2026-06-25 14:45:41 +0900

    [Passed] 길이 단위 환산하기 (1ms, 0MB)

---
+++ 260625/길이 단위 환산하기/convert-length-units.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    double FIT = 30.48;
+    double ft_val;
+    scanf("%lf", &ft_val);
+    printf("%0.1lf", FIT * ft_val);
+    
+    return 0;
+}
