703ff652630e2f2690164d9f1174d8f80a93b8b8
big20261028 <baekinki2026@gmail.com>
2026-06-29 00:59:57 +0900

    [Passed] 시험 통과 여부 확인하기 (1ms, 0MB)

---
+++ 260629/시험 통과 여부 확인하기/verify-test-passed.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int N;
+    scanf("%d", &N);
+    if (N >= 80){
+        printf("pass\n");
+    }else{
+        printf("%d more score", 80-N);
+    }
+    return 0;
+}
