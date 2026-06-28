30726c413fb4e2c4b6983ccfde7d66e73ec963d8
big20261028 <baekinki2026@gmail.com>
2026-06-29 01:10:06 +0900

    [Passed] 살 수 있는 물건 (1ms, 0MB)

---
+++ 260629/살 수 있는 물건/things-able-to-buy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int N;
+    scanf("%d", &N);
+    if (N >= 3000){
+        printf("book");
+    }else if (N >= 1000){
+        printf("mask");
+    }else{
+        printf("no");
+    }
+    return 0;
+}
