a67a1ee2f1f22645e5f87a784771d1452508aaa2
big20261028 <baekinki2026@gmail.com>
2026-06-30 21:47:56 +0900

    [Passed] 1부터 n까지 출력 (2ms, 0MB)

---
+++ 260630/1부터 n까지 출력/print-from-1-to-n.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int N;
+    scanf("%d", &N);
+    for (int i = 1; i <= N; i++){
+        printf("%d ", i);
+    }
+    return 0;
+}
