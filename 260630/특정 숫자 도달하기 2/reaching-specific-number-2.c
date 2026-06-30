97bdade27a08877b3b8ec704eff2545623f28fac
big20261028 <baekinki2026@gmail.com>
2026-06-30 22:00:32 +0900

    [Passed] 특정 숫자 도달하기 2 (2ms, 0MB)

---
+++ 260630/특정 숫자 도달하기 2/reaching-specific-number-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int N;
+    scanf("%d", &N);
+    for (N; N >= 1; N--){
+        printf("%d ", N);
+    }
+    return 0;
+}
