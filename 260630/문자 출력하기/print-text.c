bf7135b678249136f6117be3694a415a52fe3ce1
big20261028 <baekinki2026@gmail.com>
2026-06-30 22:23:47 +0900

    [Passed] 문자 출력하기 (1ms, 0MB)

---
+++ 260630/문자 출력하기/print-text.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char c;
+    scanf(" %c", &c);
+
+    int cnt = 8;
+    while (cnt >0){
+        printf("%c",c);
+        cnt--;
+    }
+    return 0;
+}
