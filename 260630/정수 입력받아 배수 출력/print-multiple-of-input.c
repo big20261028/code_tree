a75e0593e62d17da31c79e20b67ee68ed5aa4574
big20261028 <baekinki2026@gmail.com>
2026-06-30 21:57:10 +0900

    [Passed] 정수 입력받아 배수 출력 (1ms, 0MB)

---
+++ 260630/정수 입력받아 배수 출력/print-multiple-of-input.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int N;
+    scanf("%d", &N);
+    for(int i = 1; i <= 5; i++ ){
+        printf("%d ", N*i);
+    }
+    return 0;
+}
