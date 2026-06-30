300c390bcf809904692464d466a41c95d8250d2c
big20261028 <baekinki2026@gmail.com>
2026-06-30 22:08:57 +0900

    [Passed] N까지 자연수 출력 (1ms, 0MB)

---
+++ 260630/N까지 자연수 출력/print-1-to-n.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d", &n);
+    int i = 1;
+    while (i <= n){
+        printf("%d ", i++);
+    }
+    return 0;
+}
