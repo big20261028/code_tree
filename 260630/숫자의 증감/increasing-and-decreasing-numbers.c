3e23512301c12fa484297364d0d9bd8c718ab3a4
big20261028 <baekinki2026@gmail.com>
2026-06-30 22:33:51 +0900

    [Passed] 숫자의 증감 (1ms, 0MB)

---
+++ 260630/숫자의 증감/increasing-and-decreasing-numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char c;
+    int n;
+    scanf(" %c %d", &c, &n);
+    if (c == 'A'){
+        for(int i = 1; i <= n; i++){
+            printf("%d ", i);
+        }
+    }else if (c == 'D'){
+        for(n; n >= 1; n--){
+            printf("%d ", n);
+        }
+    }
+    return 0;
+}
