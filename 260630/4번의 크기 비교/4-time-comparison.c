ae7029c0aefc32c7a405b6ee8491fd798d6c419d
big20261028 <baekinki2026@gmail.com>
2026-06-30 16:41:02 +0900

    [Passed] 4번의 크기 비교 (1ms, 0MB)

---
+++ 260630/4번의 크기 비교/4-time-comparison.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int A, B, C, D, E;
+    scanf("%d %d %d %d %d", &A, &B, &C, &D, &E);
+    printf("%d\n", A>B);
+    printf("%d\n", A>C);
+    printf("%d\n", A>D);
+    printf("%d\n", A>E);
+    return 0;
+}
