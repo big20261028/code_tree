32b78bf59880c22085c04705cd8c0b1aeb9a808b
big20261028 <baekinki2026@gmail.com>
2026-06-30 23:13:45 +0900

    [Passed] A부터 B까지의 합 (2ms, 0MB)

---
+++ 260630/A부터 B까지의 합/sum-from-a-to-b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int sum = 0;
+    for(a; a<= b; a++){
+        sum += a;
+    }
+    printf("%d", sum);
+    return 0;
+}
