5d50a2328984476debeb8ffca11e8f79cafe77cb
big20261028 <baekinki2026@gmail.com>
2026-06-30 21:08:07 +0900

    [Passed] 남녀노소 구분짓기 (2ms, 0MB)

---
+++ 260630/남녀노소 구분짓기/sex-and-age.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int gender, age;
+    scanf("%d %d", &gender, &age);
+    if (gender == 0){
+        if (age >= 19){
+            printf("MAN");
+        }else{
+            printf("BOY");
+        }
+    }else{
+        if (age >= 19){
+            printf("WOMAN");
+        }else{
+            printf("GIRL");
+        }
+    }
+    return 0;
+}
