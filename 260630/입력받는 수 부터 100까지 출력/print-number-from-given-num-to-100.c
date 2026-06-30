8430ba793f154f277a6cf9a9d26fc874b7301eb5
big20261028 <baekinki2026@gmail.com>
2026-06-30 21:42:23 +0900

    [Passed] 입력받는 수 부터 100까지 출력 (2ms, 0MB)

---
+++ 260630/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d", &n);
+    for (n; n<=100; n++){
+        printf("%d ", n);
+    }
+    return 0;
+}
